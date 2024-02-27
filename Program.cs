b365a40cb5e838d8d987477ac31a77d7790cc234
DelzarKafashi <delzarkafashi@gmail.com>
2024-02-27 10:15:51 +0100

    fixat med Entitet

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+namespace labration;
+
+public class Program
+{
+    public class Entitet
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public char Symbol { get; set; }
+
+    public Entitet(int x, int y, char symbol)
+    {
+        X = x;
+        Y = y;
+        Symbol = symbol;
+    }
+}
+
+}
