e5394a753cdc5f8145df0abbeb2d24c9172a1dad
Aziz <test@gmail.com>
2024-02-27 11:00:39 +0100

    fixat varsele

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 namespace labration;
 
 public class Program
@@ -28,6 +28,26 @@ public class Spelare : Entitet
     public Spelare(int x, int y, char symbol) : base(x, y, symbol)
     {
         Livskraft = 100;
+    }
+}
+
+
+public class Varelse : Entitet
+{
+    public int Livskraft { get; set; }
+    public List<Förmåga> Förmågor { get; set; }
+    public ConsoleColor Färg { get; set; }
+
+    public Varelse(int x, int y, char symbol, ConsoleColor färg) : base(x, y, symbol)
+    {
+        Livskraft = 100;
+        Förmågor = new List<Förmåga>();
+        Färg = färg;
+    }
+
+    public void LäggTillFörmåga(Förmåga förmåga)
+    {
+        Förmågor.Add(förmåga);
     }
 }
 
