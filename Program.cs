d1c04f7e01c8df7d7cb5c7f93ec8890da0a5da11
Aziz <test@gmail.com>
2024-02-27 10:16:37 +0100

    skapat-spelare

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace labration;
+
+public class Program
+{  
+
+
+  public static void Main(string[] args)
+    {
+
+
+
+
+
+
+
+}
+}
+
+public class Entitet
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public char Symbol { get; set; }
+
+    public Entitet(int x, int y, char symbol)
+    {
+        X = x;
+        Y = y;
+        Symbol = symbol;
+    }
+}
+
+
+
+public class Spelare : Entitet
+{
+    public int Livskraft { get; set; }
+
+    public Spelare(int x, int y, char symbol) : base(x, y, symbol)
+    {
+        Livskraft = 100;
+    }
+}
